43c55571d8a8b12072808e25f70f2192415bbb63
ydmins <dr.ydmins@gmail.com>
2025-06-05 07:40:17 +0900

    feature: add test code for init_screen

---
+++ test/init_screen_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:p_20250605/main.dart';
+
+void main() {
+  testWidgets('Init Screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const UebungMachtMeister());
+
+    expect(find.text('Init Screen'), findsOneWidget);
+    expect(
+        find.text(
+            'Morgen will ich sagen genau so wie jetzt: ich übe heute auch wie ich gestern gemacht habe.'),
+        findsOneWidget);
+  });
+}
